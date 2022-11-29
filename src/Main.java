f4b8c8e71381fcdf9ef06312269d3a400b34a18d
olgazaigraewa <ol.zaigraewa@yandex.ru>
2022-11-29 13:02:56 +0300

    Задача:Положительные делители натурального числа.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int a;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите натуральное число ");
+        //Проверяем число (натуральное или нет)
+        if (scanner.hasNextInt()) {
+            a = scanner.nextInt(); //если а -натуральное число
+            System.out.println("Делителями числа  - " + a + " являются");
+            for (int i = a; i > 0; i--) {
+                int b=a%i;
+                if(b==0)
+                    System.out.println(i+" ");
+            }
+        } else {
+            System.out.println("Ошибка! Введено не натуральное число!");
+        }
     }
+
+
 }
+
+
